13d2a2b199ecbab6005b36fec144826cc74858d6
ninjaasmoke <nithins674@gmail.com>
2025-03-25 03:21:26 +0530

    Update font styles in globals.css and add Google Fonts link in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,16 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="theme-color" content="#000000" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body className={appFont.className}>
         <ThemeProvider
